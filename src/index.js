d7e79ad0f1285bd80888640453be4e00c2956a13
PenFan <fangpei9212@163.com>
2017-09-08 18:28:29 +0800

    实现回退

--- src/index.js
+++ src/index.js
@@ -47,12 +47,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     }
   }
   handleClick(i){
-    const history = this.state.history
-    const current = history[history.length -1]
+    const history = this.state.history.slice(0,this.state.stepNumber+1)
+    const current = history[history.length-1]
     const squares = current.squares.slice()
     if(calculateWinner(squares) || squares[i]){
       return
@@ -62,19 +63,28 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    })
+  }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext:( step % 2 ) ? false:true
     })
   }
   render() {
     const history = this.state.history
-    const current = history[history.length -1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
     const moves = history.map((step,move)=>{
+
       const desc = move ?
       'Move # '+ move:
       'Game Start'
       return(
-        <li>
+        <li key={move}>
           <a href="#" onClick={()=>this.jumpTo(move)}>{desc}</a>
         </li>
       )
