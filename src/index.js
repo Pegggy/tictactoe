0b0ee6587db96941df02c4d402a73a2adb23b73a
PenFan <fangpei9212@163.com>
2017-09-08 17:42:19 +0800

    展示每步链接

--- src/index.js
+++ src/index.js
@@ -11,39 +11,15 @@ function Square(props){
 }
 
 class Board extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} 
-    onClick={()=>this.handleClick(i)}/>;
+    return <Square value={this.props.squares[i]} 
+    onClick={()=>this.props.onClick(i)}/>;
   }
-  handleClick(i){
-    const squares = this.state.squares.slice()
-    if(calculateWinner(squares) || squares[i]){
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X':'O'
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    })
-  }
-  render() {
-    let status;    
-    const winner = calculateWinner(this.state.squares)
-    if(winner){
-      status = 'Winner: ' + winner 
-    }else{
-      status = 'Next player: ' + ( this.state.xIsNext?'X':'O' )
-    }
+
+  render(){
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -74,15 +50,53 @@ class Game extends React.Component {
       xIsNext: true
     }
   }
+  handleClick(i){
+    const history = this.state.history
+    const current = history[history.length -1]
+    const squares = current.squares.slice()
+    if(calculateWinner(squares) || squares[i]){
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X':'O'
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext
+    })
+  }
   render() {
+    const history = this.state.history
+    const current = history[history.length -1]
+    const winner = calculateWinner(current.squares)
+    const moves = history.map((step,move)=>{
+      const desc = move ?
+      'Move # '+ move:
+      'Game Start'
+      return(
+        <li>
+          <a href="#" onClick={()=>this.jumpTo(move)}>{desc}</a>
+        </li>
+      )
+    })
+
+
+    let status
+
+    if(winner){
+      status = 'Winner: ' + winner 
+    }else{
+      status = 'Next player: ' + ( this.state.xIsNext?'X':'O' )
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} 
+          onClick={(i)=>this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
